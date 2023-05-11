53733d4b511ae545e7b3fc7b647c3a555f12f0c3
Nemo Grippa <nemo.grippa@gmail.com>
2023-05-11 17:30:43 +0200

    It is now possible to open and close the navbar burger in mobile

--- library/NavBar.tsx
+++ library/NavBar.tsx
@@ -1,18 +1,27 @@
+import {inspect} from "util";
+import styles from "./../components/layout.module.css";
+import {useState} from "react";
+import clsx from "clsx";
+
 export const NavBar = () => {
+    const [isActive, setIsActive] = useState<boolean>(false);
+
   return (
-    <nav className="navbar is-light" role="navigation" aria-label="main navigation">
+    <nav className="navbar has-shadow is-white" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <a className="navbar-item" href="https://marvel-filme.fandom.com/de/wiki/Scarlet_Witch">
           <img
-            src="/../public/Marvel-Comics-Logo.svg"
-            // width="112" //411
-            // height="28" //186
+            src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Marvel_Logo.svg"
+            // override the bulma default
+            style={{maxHeight: 60}}
           />
         </a>
 
         <a
           role="button"
-          className="navbar-burger"
+          className={`navbar-burger burger ${isActive ? 'is-active' : ''}`}
+          onClick={() => {setIsActive(!isActive)}}
+          id="burger"
           aria-label="menu"
           aria-expanded="false"
           data-target="navbarBasicExample"
@@ -23,22 +32,13 @@ export const NavBar = () => {
         </a>
       </div>
 
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-center">
-          <a className="navbar-item" href={"/"}>Home</a>
-
-          <a className="navbar-item" href={"/story"}>Geschichte</a>
-
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">More</a>
-
-            <div className="navbar-dropdown">
-              <a className="navbar-item" href={"superpower"}>Superkräfte</a>
-              <a className="navbar-item" href={"trivia"}>Trivia</a>
-              <hr className="navbar-divider" />
-              <a className="navbar-item" href={"pictures"}>Bilder</a>
-            </div>
-          </div>
+      <div id="navBar-menu" className={`navbar-menu ${isActive ? "is-active" : ""}`} onClick={() => {setIsActive(!isActive)}}>
+        <div className="navbar-end">
+            <a className="navbar-item" href={"/"}>Home</a>
+            <a className="navbar-item" href={"/story"}>Geschichte</a>
+            <a className="navbar-item" href={"superpower"}>Superkräfte</a>
+            <a className="navbar-item" href={"trivia"}>Trivia</a>
+            <a className="navbar-item" href={"pictures"}>Bilder</a>
         </div>
       </div>
     </nav>
