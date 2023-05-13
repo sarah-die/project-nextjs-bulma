a2184f0a839a1afec7faf414fd68601fe9065294
sarah-die <saridi@alice.de>
2023-05-13 12:31:54 +0200

    Spacing and Containers

---
+++ src/pages/spacingAndContainers.tsx
@@ -0,0 +1,34 @@
+import Layout from "../../components/layout";
+import Head from "next/head";
+
+export default function spacingAndContainers() {
+  return (
+    <Layout>
+      <Head>
+        <title>Spacing and Containers</title>
+      </Head>
+      <h1 className="pt-3 ml-3">Hello guys. pt-3 ml-3</h1>
+      <h1 className="py-1 my-3">Hello guys. py-1 my-3</h1>
+      <h1 className="px-4 mx-6">Hello guys. px-4 mx-6</h1>
+      <h1 className="p-3 m-3">Hello guys. p-3 m-3</h1>
+      <section className="section">
+        <p> This is some text to say "hello" to you. </p>
+      </section>
+      <div className="section has-background-success">
+        <div className="container">
+          type lorem30 and press tab to get a lorem with 30 words. Add
+          className="container" to get auto margin and a maxWidth
+        </div>
+      </div>
+      <div className="section">
+        <div className="container">
+          <p>
+            This is a section with a container inside and some Lorem ipsum dolor
+            sit amet, consectetur adipisicing elit. Alias amet blanditiis
+            consequuntur dolore! Accusantium, ipsum?
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+}
