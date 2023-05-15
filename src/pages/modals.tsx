2f44d52535faaf36a4f797b554bb1b73a78d4670
sarah-die <saridi@alice.de>
2023-05-15 11:49:46 +0200

    Form fields (text input, checkbox)

--- src/pages/modals.tsx
+++ src/pages/modals.tsx
@@ -25,6 +25,35 @@ export default function Modals() {
         ></div>
         <div className="modal-content has-background-white py-5 px-5">
           <h3 className="title mb-6">Join the club</h3>
+          <form>
+            {/* Each form field has to be surrounded by a div.field. Applies default spacing. */}
+            <div className="field">
+              <label className="label">Name</label>
+              {/* whenever user has control over information e.g. input fields, surround them by control-class*/}
+              <div className="control">
+                <input type="text" className="input" placeholder="Name" />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">E-Mail</label>
+              {/* whenever user has control over information e.g. input fields, surround them by control-class*/}
+              <div className="control">
+                <input type="text" className="input" placeholder="E-Mail" />
+              </div>
+            </div>
+            {/*  Important: Follow those styles down below to style it in the correct way.*/}
+            <div className="field">
+              <div className="control">
+                <label className="checkbox">
+                  <input type="checkbox" /> I agree to the{" "}
+                  <a href="">terms and conditions.</a>
+                </label>
+              </div>
+            </div>
+            <div className="mt-6 has-text-centered">
+              <button className="button is-warning">Sign Up</button>
+            </div>
+          </form>
         </div>
       </div>
     </Layout>
